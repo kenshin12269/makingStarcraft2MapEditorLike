1378eb430777a148968d32b0fe6a0d86b4142799
kimsungjin <kimsungjin@doubleugames.com>
2021-05-07 21:00:18 +0900

    feat: testing

--- Assets/CustomEditor/Editor/MapEditor.cs
+++ Assets/CustomEditor/Editor/MapEditor.cs
@@ -36,38 +36,58 @@ public class MapEditor : EditorWindow
     void OnGUI()
     {
         Event e = Event.current;
-        if (e.type == EventType.ContextClick)
-        {
-            var menu = new GenericMenu();
-            menu.AddItem(new GUIContent("Hello"), true, () => Debug.LogFormat("Hello"));
-            menu.AddSeparator("");
-            menu.AddItem(new GUIContent("Togepi"), false, () => Debug.LogFormat("Hello222"));
-            menu.ShowAsContext();
-        }
-        //Debug.LogFormat("EventType={0}", e.type.ToString());
-        GUILayout.Space(8);
-        //Debug.LogFormat("Position={0}", this.position);
-        using (new GUILayout.HorizontalScope())
-        {
-            var listOfTools = new List<GUIContent>();
-            listOfTools.Add(new GUIContent("111"));
-            listOfTools.Add(new GUIContent("222"));
-            listOfTools.Add(new GUIContent("333"));
-            int toolBarIndex = selectedToolBar;
-            EditorGUI.BeginChangeCheck();
-            toolBarIndex = GUILayout.Toolbar(toolBarIndex, listOfTools.ToArray());
-            if (EditorGUI.EndChangeCheck())
-            {
-                Debug.LogFormat("Changed?={0}", toolBarIndex);
-                selectedToolBar = toolBarIndex;
-            }
-        }
 
-        using (new GUILayout.VerticalScope("box"))
-        {
-            EditorGUILayout.LabelField("One");
-            EditorGUILayout.LabelField("Two");
-        }
+        // GUI.Button(new Rect(10, 10, 100, 100), "Hello");
+        // if (e.type == EventType.Repaint)
+        //     GUI.skin.button.Draw(new Rect(10, 10, 100, 100), false, false, false, false);
+
+        // int controlID = GUIUtility.GetControlID("Test.Test".GetHashCode(), FocusType.Passive, new Rect(10, 10, 100, 100));
+        // var typeOfControl = Event.current.GetTypeForControl(controlID);
+
+        // Debug.LogFormat("GotEvent={0} ControlEvent={1} Cnt={2}", e.type, typeOfControl, Event.GetEventCount());
+
+
+        // return;
+
+        // if (e.type == EventType.ContextClick)
+        // {
+        //     var menu = new GenericMenu();
+        //     menu.AddItem(new GUIContent("Hello"), true, () => Debug.LogFormat("Hello"));
+        //     menu.AddSeparator("");
+        //     menu.AddItem(new GUIContent("Togepi"), false, () => Debug.LogFormat("Hello222"));
+        //     menu.ShowAsContext();
+        // }
+        // //Debug.LogFormat("EventType={0}", e.type.ToString());
+        // GUILayout.Space(8);
+        // //        Debug.LogFormat("Tool={0}", Tools.current);
+        // //Debug.LogFormat("Position={0}", this.position);
+        // using (new GUILayout.HorizontalScope())
+        // {
+        //     var listOfTools = new List<GUIContent>();
+        //     listOfTools.Add(new GUIContent("111"));
+        //     listOfTools.Add(new GUIContent("222"));
+        //     listOfTools.Add(new GUIContent("333"));
+        //     int toolBarIndex = selectedToolBar;
+        //     EditorGUI.BeginChangeCheck();
+        //     toolBarIndex = GUILayout.Toolbar(toolBarIndex, listOfTools.ToArray());
+        //     if (EditorGUI.EndChangeCheck())
+        //     {
+        //         // Debug.LogFormat("Changed?={0}", toolBarIndex);
+        //         selectedToolBar = toolBarIndex;
+        //     }
+        // }
+
+        // using (new GUILayout.VerticalScope("box"))
+        // {
+        //     EditorGUILayout.LabelField("One");
+        //     EditorGUILayout.LabelField("Two");
+        // }
+
+        // Rect position = GUILayoutUtility.GetRect(MyGUI.TempContent("Hello"), GUI.skin.button);
+        MyGUI.Button(new Rect(10, 10, 100, 100), MyGUI.TempContent("Hello"), GUI.skin.button);
+        MyGUI.Button(new Rect(50, 50, 100, 100), MyGUI.TempContent("Hello"), GUI.skin.button);
+        // Debug.LogFormat("IsPressed={0}", isPressed);
+        // GUILayout.Button("OkayBtn");
     }
     void OnSceneGUI(SceneView sceneView)
     {
@@ -79,17 +99,83 @@ public class MapEditor : EditorWindow
     }
     void OnSelectionChanged()
     {
-        // We want to delete deselected gameObjects from this.m_Hovering
-        // var toDelete = new List<GameObject>();
-        // var selectionGameObjects = Selection.gameObjects;
 
-        // foreach (var hovering in m_Hovering.Keys)
-        //     if (!selectionGameObjects.Contains(hovering))
-        //         toDelete.Add(hovering);
+    }
+}
 
-        // foreach (var go in toDelete)
-        //     m_Hovering.Remove(go);
+public static class MyGUI
+{
+    private static readonly GUIContent s_TempContent = new GUIContent();
+    public static GUIContent TempContent(string text)
+    {
+        s_TempContent.text = text;
+        s_TempContent.image = null;
+        s_TempContent.tooltip = null;
+        return s_TempContent;
+    }
 
-        // m_IgnoreDrag.Clear();
+    private static readonly int s_ButtonHint = "MyGUI.Button".GetHashCode();
+    public static bool Button(Rect position, GUIContent label, GUIStyle style)
+    {
+        int controlID = GUIUtility.GetControlID(s_ButtonHint, FocusType.Passive, position);
+        bool result = false;
+        var typeOfControl = Event.current.GetTypeForControl(controlID);
+        if (Event.current.type != EventType.Repaint && Event.current.type != EventType.Layout)
+            Debug.LogFormat("ControlID={0} tof={1} cur={2}", controlID, typeOfControl, Event.current.type);
+        switch (typeOfControl)
+        {
+            case EventType.MouseDown:
+                if (GUI.enabled && position.Contains(Event.current.mousePosition))
+                {
+                    GUIUtility.hotControl = controlID;
+                    Event.current.Use();
+                }
+                break;
+            // case EventType.MouseDrag:
+            //     if (GUIUtility.hotControl == controlID)
+            //     {
+            //         Event.current.Use();
+            //     }
+            //     break;
+            case EventType.MouseUp:
+                if (GUIUtility.hotControl == controlID)
+                {
+                    GUIUtility.hotControl = 0;
+                    Event.current.Use();
+                    if (position.Contains(Event.current.mousePosition))
+                    {
+                        result = true;
+                    }
+                }
+                break;
+            case EventType.KeyDown:
+                {
+                    if (GUIUtility.hotControl == controlID)
+                    {
+                        if (Event.current.keyCode == KeyCode.Escape)
+                        {
+                            GUIUtility.hotControl = 0;
+                            Event.current.Use();
+                        }
+                    }
+                }
+                break;
+            case EventType.Repaint:
+                {
+                    style.Draw(position, label, controlID, false, position.Contains(Event.current.mousePosition));
+                    result = controlID == GUIUtility.hotControl && position.Contains(Event.current.mousePosition);
+                }
+                break;
+        }
+        return result;
+    }
+
+    public static bool Button(Rect position, string label, GUIStyle style)
+    {
+        return Button(position, TempContent(label), style);
+    }
+    public static bool Button(Rect position, string label)
+    {
+        return Button(position, label, GUI.skin.button);
     }
 }
