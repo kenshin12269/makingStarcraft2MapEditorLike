cfcdeff3df5d19a59285da1b19355ef3cfc08422
kimsungjin <kimsungjin@doubleugames.com>
2021-06-18 14:52:21 +0900

    wefewfwe

--- Assets/CustomEditor/Editor/MapEditor.cs
+++ Assets/CustomEditor/Editor/MapEditor.cs
@@ -11,16 +11,17 @@ public class MapEditor : EditorWindow
     }
 
     static readonly Vector2 k_EditorWindowMinimumSize = new Vector2(320, 180);
-
+    GUISkin mySkin;
     void OnEnable()
     {
         minSize = k_EditorWindowMinimumSize;
         titleContent = new GUIContent("MapEditor");
         SceneView.duringSceneGui += OnSceneGUI;
         Undo.undoRedoPerformed += UndoRedoPerformed;
-        //wantsMouseMove = true;
+        // wantsMouseMove = true;
         //EditorGUIUtility.TrIconContent(IconUtility.GetIcon)
         Selection.selectionChanged += OnSelectionChanged;
+        mySkin = AssetDatabase.LoadAssetAtPath<GUISkin>("Assets/CustomEditor/MySkin.guiskin");
     }
     void OnDisable()
     {
@@ -32,71 +33,127 @@ public class MapEditor : EditorWindow
     {
         Debug.LogFormat("Destroyed");
     }
-    int selectedToolBar;
-    Color newColor;
+
     void OnGUI()
     {
-        Event e = Event.current;
-
-        // GUI.Button(new Rect(10, 10, 100, 100), "Hello");
-        // if (e.type == EventType.Repaint)
-        //     GUI.skin.button.Draw(new Rect(10, 10, 100, 100), false, false, false, false);
-
-        // int controlID = GUIUtility.GetControlID("Test.Test".GetHashCode(), FocusType.Passive, new Rect(10, 10, 100, 100));
-        // var typeOfControl = Event.current.GetTypeForControl(controlID);
-
-        // Debug.LogFormat("GotEvent={0} ControlEvent={1} Cnt={2}", e.type, typeOfControl, Event.GetEventCount());
-
-
-        // return;
-
-        // if (e.type == EventType.ContextClick)
-        // {
-        //     var menu = new GenericMenu();
-        //     menu.AddItem(new GUIContent("Hello"), true, () => Debug.LogFormat("Hello"));
-        //     menu.AddSeparator("");
-        //     menu.AddItem(new GUIContent("Togepi"), false, () => Debug.LogFormat("Hello222"));
-        //     menu.ShowAsContext();
-        // }
-        // //Debug.LogFormat("EventType={0}", e.type.ToString());
-        // GUILayout.Space(8);
-        // //        Debug.LogFormat("Tool={0}", Tools.current);
-        // //Debug.LogFormat("Position={0}", this.position);
-        // using (new GUILayout.HorizontalScope())
-        // {
-        //     var listOfTools = new List<GUIContent>();
-        //     listOfTools.Add(new GUIContent("111"));
-        //     listOfTools.Add(new GUIContent("222"));
-        //     listOfTools.Add(new GUIContent("333"));
-        //     int toolBarIndex = selectedToolBar;
-        //     EditorGUI.BeginChangeCheck();
-        //     toolBarIndex = GUILayout.Toolbar(toolBarIndex, listOfTools.ToArray());
-        //     if (EditorGUI.EndChangeCheck())
-        //     {
-        //         // Debug.LogFormat("Changed?={0}", toolBarIndex);
-        //         selectedToolBar = toolBarIndex;
-        //     }
-        // }
-
-        // using (new GUILayout.VerticalScope("box"))
-        // {
-        //     EditorGUILayout.LabelField("One");
-        //     EditorGUILayout.LabelField("Two");
-        // }
-
-        // Rect position = GUILayoutUtility.GetRect(MyGUI.TempContent("Hello"), GUI.skin.button);
-        MyGUI.Button(new Rect(10, 10, 100, 100), MyGUI.TempContent("Hello"), GUI.skin.button);
-        MyGUI.Button(new Rect(50, 50, 100, 100), MyGUI.TempContent("Hello"), GUI.skin.button);
-        // newColor = EditorGUI.ColorField(new Rect(10, 10, 100, 50), GUIContent.none, newColor, true, true, false);
-        // Repaint();
-        // if (Event.current.type != EventType.Repaint && Event.current.type != EventType.Layout)
-        // Debug.LogFormat("cur={0}", Event.current.type);
-        // Debug.LogFormat("IsPressed={0}", isPressed);
-        // GUILayout.Button("OkayBtn");
+        GUI.skin.button.Draw(new Rect(10, 10, 100, 100), "This Default Style button emits EventType.Repaint when cursor enters", false, false, false, false);
+        mySkin.button.Draw(new Rect(100, 10, 100, 100), "This Custom Style button does not", false, false, false, false);
     }
+    void DoCustomButton(Rect position, string name)
+    {
+        int controlID = GUIUtility.GetControlID("TmpBtn".GetHashCode(), FocusType.Passive, position);
+        switch (Event.current.GetTypeForControl(controlID))
+        {
+            case EventType.Repaint:
+                {
+                    GUI.skin.button.Draw(position, GUIContent.none, controlID, false, position.Contains(Event.current.mousePosition));
+                    // Debug.LogFormat("ButtonType={0}", GUI.skin.button.GetType().ToString());
+                }
+                break;
+            default:
+                // Debug.LogFormat("Event={0}", Event.current.type);
+                break;
+        }
+    }
+    void DoMyWindow(int windowID)
+    {
+        // GUI.Button(new Rect(0, 0, 100, 100), "Btn");
+        // for (int i = 0; i < 50; i++)
+        GUILayout.Button("One");
+        // GUILayout.Button("Two");
+    }
+    bool isDragging = false;
+    Vector2 startPos;
+    Vector2 endPos;
+    public static string GetColor(int value)
+    {
+        System.Random rand = new System.Random(value * 1324);
+        var aCol = rand.Next(0, int.MaxValue);
+        Color32 c = default;
+        c.b = (byte)((aCol) & 0xFF);
+        c.g = (byte)((aCol >> 8) & 0xFF);
+        c.r = (byte)((aCol >> 16) & 0xFF);
+        // c.r = 0;
+        // c.g = 100;
+        // c.b = 150;
+        c.a = 255;
+        // c.a = (byte)((aCol >> 24) & 0xFF);
+        return string.Format("{0}{1}{2}FF", c.r.ToString("X"), c.g.ToString("X"), c.b.ToString("X"));
+    }
+    void MovableSphere(Vector3 position, float radius)
+    {
+        var controlID = GUIUtility.GetControlID("CustomControl".GetHashCode(), FocusType.Passive);
+        var handleEventType = Event.current.GetTypeForControl(controlID);
+        switch (handleEventType)
+        {
+            case EventType.Layout:
+            case EventType.MouseMove:
+                {
+                    var screenPos = Handles.matrix.MultiplyPoint(position);
+                    //This
+                    HandleUtility.AddControl(controlID, HandleUtility.DistanceToCircle(position, radius));
+                }
+                break;
+            case EventType.MouseDown:
+                if (HandleUtility.nearestControl == controlID && GUIUtility.hotControl == 0)
+                {
+                    GUIUtility.hotControl = controlID;
+                }
+                break;
+            case EventType.MouseDrag:
+                if (GUIUtility.hotControl == controlID)
+                {
+                    Event.current.Use();
+                }
+                break;
+            case EventType.MouseUp:
+                {
+                    if (GUIUtility.hotControl == controlID)
+                    {
+                        GUIUtility.hotControl = 0;
+                    }
+                }
+                break;
+            case EventType.Repaint:
+                {
+                    if (HandleUtility.nearestControl == controlID && GUIUtility.hotControl == 0)
+                    {
+                        var beforeColor = Handles.color;
+                        Handles.color = Color.red;
+                        Handles.SphereHandleCap(controlID, position, Quaternion.identity, radius, handleEventType);
+                        Handles.color = beforeColor;
+                    }
+                    else
+                    {
+                        Handles.SphereHandleCap(controlID, position, Quaternion.identity, radius, handleEventType);
+                    }
+                }
+                break;
+        }
+    }
+    void LogFormat(EventType eventType, string str, params object[] args)
+    {
+        var convertedColor = GetColor((int)eventType);
+        Debug.LogFormat("<color=#{0}>{1}</color>\t:{2}", convertedColor, eventType, string.Format(str, args));
+    }
+    [SerializeField] Vector3 handlePos;
     void OnSceneGUI(SceneView sceneView)
     {
         //Debug.LogFormat("CAlled");
+        // var controlID = GUIUtility.GetControlID("CustomControl".GetHashCode(), FocusType.Passive);
+        // var handleEventType = Event.current.GetTypeForControl(controlID);
+        // LogFormat(handleEventType, "{0}", Event.current.mousePosition);
+
+        // MovableSphere(new Vector3(0, 0, 0), 0.5f);
+        // MovableSphere(new Vector3(3, 0, 0), 0.5f);
+
+        // EditorGUI.BeginChangeCheck();
+        // var newHandlePos = Handles.Slider(handlePos, Vector3.right);
+        // if (EditorGUI.EndChangeCheck() == true)
+        // {
+        //     Undo.RecordObject(this, "TEst");
+        //     handlePos = newHandlePos;
+        // }
     }
     void UndoRedoPerformed()
     {
@@ -126,7 +183,7 @@ public static class MyGUI
         bool result = false;
         var typeOfControl = Event.current.GetTypeForControl(controlID);
         // var anotherTypeOfControl = Event.current.GetTypeForControl(1132);
-        // Debug.LogFormat("Event={0} TypeForControl={1} Another={2}", Event.current.type, typeOfControl, anotherTypeOfControl);
+        Debug.LogFormat("Event={0} TypeForControl={1}", Event.current.type, typeOfControl);
 
 
         // if (Event.current.type != EventType.Repaint && Event.current.type != EventType.Layout)
---
+++ Assets/CustomEditor/Editor/TestSomethingInspector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TestSomething))]
+public class TestSomethingInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI();
+        TestSomething tmp = target as TestSomething;
+        tmp.buttonStyle = GUI.skin.button;
+        // EditorGUILayout.ObjectField(GUI.skin.button.normal.background, typeof(Texture2D));
+        tmp.mySkin = GUI.skin;
+        if (GUILayout.Button("Copy") == true)
+        {
+            AssetDatabase.CreateAsset(GUI.skin, "Assets/myskin.guiskin");
+            AssetDatabase.Refresh();
+        }
+        // Debug.LogFormat("Background={0}",GUI.skin.button.normal.background)
+        base.OnInspectorGUI();
+
+        // serializedObject.Update();
+
+        // var buttonStyleProp = serializedObject.FindProperty("buttonStyle");
+        // Debug.LogFormat("WTF={0}", buttonStyleProp.GetType().Name);
+        // //        EditorGUILayout.PropertyField(serializedObject.FindProperty("buttonStyle"))
+
+        // serializedObject.ApplyModifiedProperties();
+    }
+}
